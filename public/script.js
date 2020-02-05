a23bdc1ccccd9029c758aae18b4fde9811671e56
ali-7 <ali.7@outlook.com>
2020-02-06 01:47:21 +0200

    update-script.js

--- public/script.js
+++ public/script.js
@@ -1,32 +1,33 @@
-document.onreadystatechange = function () {
+document.onreadystatechange = () => {
   if (document.readyState === 'complete') {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-      if(xhr.readyState === 4){
-        if (xhr.status === 200){
-          var data = JSON.parse(xhr.responseText);
-          for (var blogPost in data) {
-            var postDiv         = document.createElement('div');
-            var postText        = document.createElement('p');
-            var thumbnail       = document.createElement('img');
-            var postContainer   = document.getElementsByClassName('post-container')[0];
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          const data = JSON.parse(xhr.responseText);
+          for (let blogPost in data) {
+            const postDiv = document.createElement('div');
+            const postText = document.createElement('p');
+            const thumbnail = document.createElement('img');
+            const postContainer = document.getElementsByClassName(
+              'post-container'
+            )[0];
 
-            thumbnail.src = "./img/logo2.png";
-            thumbnail.className = "thumbnail";
+            thumbnail.src = './public/img/logo2.png';
+            thumbnail.className = 'thumbnail';
             postText.innerHTML = data[blogPost];
-            postDiv.className = "post";
+            postDiv.className = 'post';
 
             postDiv.appendChild(thumbnail);
             postDiv.appendChild(postText);
             postContainer.appendChild(postDiv);
           }
-        }
-        else {
+        } else {
           console.error(xhr.responseText);
         }
       }
-    }
+    };
     xhr.open('GET', '/posts', true);
     xhr.send();
   }
-}
+};
